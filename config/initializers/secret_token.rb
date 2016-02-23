f37a18fa0d0697be3cfb4a2206eb80afc93fafb1
yft <yftang@freewheel.tv>
2016-02-23 20:15:40 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToyApp::Application.config.secret_key_base = 'bafef1a1907c72e028af5a43ffa2a35cf6f9efb137036f448d3f09a3cecee643d6266d3528dc4d805dbaacfba7d127d5a257bfb7a870e535e871c42ca8aadcf0'
